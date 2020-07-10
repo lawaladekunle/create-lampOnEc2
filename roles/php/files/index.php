fe72d6dca9eebec96203053ce29d79881feecbd4
Adekunle <lawaladekunle@gmail.com>
2020-07-10 08:32:53 +0100

    Adding Ansible Roles and Playbook.

---
+++ roles/php/files/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Welcome to DevOps Solar System </title>
+</head>
+<h1><?php echo "Welcome to the DevOps Solar System." ?></h1>
+<body> <?php phpinfo() ?> </body>
+
+</html>
